5afb5176d666cc9d71449c810bd9f6fe50af9a91
Alang Thi Kiet <kiet.alang@pn.com.vn>
2019-09-11 09:39:01 +0700

    change text by java

--- app/src/main/java/com/example/textview/MainActivity.java
+++ app/src/main/java/com/example/textview/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView hello;
+        hello = findViewById(R.id.hello);
+        hello.setText("Code java");
+        hello.setTextColor(Color.GREEN);
     }
 }
